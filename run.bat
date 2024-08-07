50c4a4ff86e7dbf3f031c59ad680b1916d95278f
ilhamdika <ilham27dika@gmail.com>
2024-08-07 16:05:18 +0700

    modif bat

--- run.bat
+++ run.bat
@@ -1,3 +1,22 @@
 @echo off
-python youtube.py
+:menu
+echo Pilih opsi:
+echo 1. Like subscribe watch
+echo 2. Like
+echo 3. Watch
+echo 4. Subscribe
+set /p choice=Masukkan pilihan Anda (1/2/3/4):
+
+if "%choice%"=="1" (
+    python like_subscribe_watch.py
+) else if "%choice%"=="2" (
+    python like.py
+) else if "%choice%"=="3" (
+    python watch.py
+) else if "%choice%"=="4" (
+    python subscribe.py
+) else (
+    echo Pilihan tidak valid, coba lagi.
+    goto menu
+)
 pause
