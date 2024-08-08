0df307a19b135a290c49f8474236afdc5dbd08b7
ilhamdika <ilham27dika@gmail.com>
2024-08-08 10:36:16 +0700

    modif bat

--- run.bat
+++ run.bat
@@ -1,22 +1,65 @@
 @echo off
+color 0A
+
+cls
+echo ============================================
+echo               Hallo
+echo ============================================
+echo.
+
 :menu
 echo Pilih opsi:
-echo 1. Like subscribe watch
+echo 1. Like, Subscribe, Watch
 echo 2. Like
 echo 3. Watch
 echo 4. Subscribe
-set /p choice=Masukkan pilihan Anda (1/2/3/4):
+echo.
+
+set /p choice=Masukkan pilihan sesuai dengan angka (1/2/3/4):
 
 if "%choice%"=="1" (
-    python like_subscribe_watch.py
+    set script=like_subscribe_watch.py
 ) else if "%choice%"=="2" (
-    python like.py
+    set script=like.py
 ) else if "%choice%"=="3" (
-    python watch.py
+    set script=watch.py
 ) else if "%choice%"=="4" (
-    python subscribe.py
+    set script=subscribe.py
 ) else (
-    echo Pilihan tidak valid, coba lagi.
+    echo.
+    echo [ERROR] Pilihan tidak valid, coba lagi.
+    echo.
     goto menu
 )
-pause
+
+:confirm
+echo ============================================
+echo Anda memilih untuk menjalankan %script%.
+echo ============================================
+echo Apakah Anda yakin? (y/n)
+echo.
+
+echo Jika ya isi(y) jika tidak isi(n)
+set /p confirm=Masukkan pilihan (y/n):
+
+if "%confirm%"=="y" (
+    echo.
+    echo Menjalankan %script%...
+    python %script%
+    echo.
+    echo Proses selesai.
+    echo.
+    goto menu
+) else if "%confirm%"=="n" (
+    echo.
+    echo Anda membatalkan pilihan. Kembali ke menu...
+    echo.
+    cls
+    goto menu
+) else (
+    echo.
+    echo [ERROR] Pilihan tidak valid, coba lagi.
+    echo.
+    goto confirm
+)
+
